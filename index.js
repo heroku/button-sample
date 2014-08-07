b1efc54db02ae8d51c4349fd9dd4a7a73363a0ad
Michael <friism@gmail.com>
2014-08-07 05:55:52 -0700

    serve some more static html

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.render('index.html')
 })
 
 app.listen(app.get('port'), function() {
